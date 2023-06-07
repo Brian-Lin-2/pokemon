5c87ffb514a75f44f60241568ce20fadcb068bfc
Brian <linbrian24@gmail.com>
2023-06-06 22:52:27 -0400

    Adjust hero position and add shadow image

--- Overworld.js
+++ Overworld.js
@@ -16,8 +16,8 @@ class Overworld {
     const hero = new Image();
     hero.src = "/images/characters/people/red.png";
 
-    const x = 0;
-    const y = 0;
+    const x = 2;
+    const y = 5;
 
     hero.onload = () => {
       this.ctx.drawImage(
@@ -26,11 +26,28 @@ class Overworld {
         0, // right
         64, // width
         64, // height
-        x,
-        y,
+        x * 16 - 7, // 16 compensates for the map size
+        y * 16 - 18,
         32, // width (size)
         32, // height (size)
       );
     }
+
+    const shadow = new Image();
+    shadow.src = "/images/characters/shadow.png"
+
+    shadow.onload = () => {
+      this.ctx.drawImage(
+        shadow,
+        0, // left
+        0, // right
+        32, // width
+        32, // height
+        x * 16 - 7, // 16 compensates for the map size
+        y * 16 - 18,
+        32, // width (size)
+        32, // height (size)
+      );
+    };
   };
 };
