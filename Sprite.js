b77e0f958e1313126110fbb11db933422943378b
Brian <linbrian24@gmail.com>
2023-06-07 12:01:30 -0400

    Add shadows

--- Sprite.js
+++ Sprite.js
@@ -7,12 +7,20 @@ class Sprite {
       this.isLoaded = true;
     }
 
+    // Shadow.
+    this.shadow = new Image();
+    this.shadow.src = "/images/characters/shadow.png";
+    this.shadow.onload = () => {
+      this.isShadowLoaded = true;
+    }
+
     // Configure Animation and Initial State.
     this.animations = config.animations || {
       idleDown: [
         [0, 0]
       ]
     }
+
     this.currentAnimation = config.currentAnimation || "idleDown";
     this.currentAnimationFrame = 0;
     this.gameObject = config.gameObject;
@@ -21,6 +29,8 @@ class Sprite {
   draw(ctx) {
     const x = this.gameObject.x * 16 - 7;
     const y = this.gameObject.y * 16 -18;
+
+    this.isShadowLoaded && ctx.drawImage(this.shadow, x, y);
 
     ctx.drawImage(this.image,
       0, 0, // left, top
