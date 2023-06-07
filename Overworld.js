b20e4a92e2f1feec497edfb3166a7fe77d02193c
Brian <linbrian24@gmail.com>
2023-06-07 12:24:27 -0400

    Finish implementing game loop feature

--- Overworld.js
+++ Overworld.js
@@ -3,10 +3,22 @@ class Overworld {
     this.element = config.element;
     this.canvas = this.element.querySelector(".game-canvas");
     this.ctx = this.canvas.getContext("2d");
+    this.map = null;
   }
 
   startGameLoop() {
     const step = () => {
+      // Clears the map.
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+      this.map.drawLowerImage(this.ctx);
+
+      // Object.values() converts an object into an array.
+      Object.values(this.map.gameObjects).forEach(object => {
+        object.sprite.draw(this.ctx);
+      })
+      this.map.drawUpperImage(this.ctx);
+
       // Fires after a new frame is loaded.
       requestAnimationFrame(() => {
         step();
@@ -16,31 +28,7 @@ class Overworld {
   }
 
   init() {
-    // Map.
-    const image = new Image();
-    image.src = "/images/maps/DemoLower.png";
-
-    image.onload = () => {
-      this.ctx.drawImage(image, 0, 0);
-    };
-
-    // Places Game Objects.
-    const red = new GameObject({
-      x: 5,
-      y: 6,
-      src: "/images/characters/people/red.png"
-    })
-    
-    const mom = new GameObject({
-      x: 2,
-      y: 6,
-      src: "images/characters/people/mom.png"
-    })
-
-    // Make async so the sprites can load.
-    setTimeout(() => {
-      red.sprite.draw(this.ctx);
-      mom.sprite.draw(this.ctx);
-    }, 200)
+    this.map = new Map(window.Maps.PalletTown);
+    this.startGameLoop();
   }
-};
+}
