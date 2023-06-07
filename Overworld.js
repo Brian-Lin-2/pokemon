32b9d580baabc9f3f572d4f304aee7977e5ad84c
Brian <linbrian24@gmail.com>
2023-06-06 22:46:07 -0400

    Add hero

--- Overworld.js
+++ Overworld.js
@@ -12,5 +12,25 @@ class Overworld {
     image.onload = () => {
       this.ctx.drawImage(image, 0, 0);
     };
+
+    const hero = new Image();
+    hero.src = "/images/characters/people/red.png";
+
+    const x = 0;
+    const y = 0;
+
+    hero.onload = () => {
+      this.ctx.drawImage(
+        hero,
+        0, // left
+        0, // right
+        64, // width
+        64, // height
+        x,
+        y,
+        32, // width (size)
+        32, // height (size)
+      );
+    }
   };
-}
+};
